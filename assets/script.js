8bb3015cb22bdfd4341dee81752913ab285d35b9
Conor Kelly <conorjkelly96@gmail.com>
2021-10-03 20:02:37 +0100

    game over added

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,35 @@ const startQuizHeader = document.getElementById("start-quiz-header");
 const startQuizButtonDiv = document.getElementById("start-button-div");
 const startQuizButton = document.getElementById("startbtn");
 const headerContainer = document.getElementById("header");
+const timerElement = document.createElement("div");
 
 let countdownClock = 60;
 
 const renderTimer = function () {
-  const timerElement = document.createElement("div");
   timerElement.textContent = "Time Remaining " + countdownClock;
   timerElement.setAttribute("class", "header-content");
   timerElement.setAttribute("id", "clock");
   headerContainer.appendChild(timerElement);
 };
 
+const renderQuizOver = function () {
+  const gameOverDiv = document.createElement("div");
+  gameOverDiv.setAttribute("class", "");
+};
+
 const startTimer = function () {
-  const timerElement = document.getElementById;
+  const timerElement = document.querySelector("#clock");
+
+  const timerTick = function () {
+    if (countdownClock <= 0) {
+      clearInterval(timer);
+
+      const quizContainerDiv = document.querySelector("#quiz-container");
+      quizContainerDiv.remove();
+
+      render;
+    }
+  };
 };
 
 const renderQuestions = function () {};
