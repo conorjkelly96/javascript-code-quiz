09d699647af00d06ea4250092838369326870e7c
Conor Kelly <conorjkelly96@gmail.com>
2021-10-03 08:37:00 +0100

    start quiz elements declared

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+// Declaring Element Variables Globally
+const startQuizDiv = document.getElementById("start-quiz-div");
+const startQuizHeader = document.getElementById("start-quiz-header");
+const startQuizButtonDiv = document.getElementById("start-button-div");
+const startQuizButton = document.getElementById("start-button");
