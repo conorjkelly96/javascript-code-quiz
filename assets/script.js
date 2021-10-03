aefb238ac2f39410bc6ab599e52adb837d4be393
Conor Kelly <conorjkelly96@gmail.com>
2021-10-03 09:02:02 +0100

    renderTimer function

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,34 @@
 const startQuizDiv = document.getElementById("start-quiz-div");
 const startQuizHeader = document.getElementById("start-quiz-header");
 const startQuizButtonDiv = document.getElementById("start-button-div");
-const startQuizButton = document.getElementById("start-button");
+const startQuizButton = document.getElementById("startbtn");
+
+let countdownClock = 60;
+
+const renderTimer = function () {
+  const timerElement = document.createElement("div");
+  timerElement.textContent = "Time Remaining" + countdownClock;
+  timerElement.setAttribute("class", "header-content");
+  timerElement.setAttribute("id", "clock");
+
+  startQuizHeader.appendChild(timerElement);
+};
+
+const startTimer = function () {
+  const timerElement = document.getElementById;
+};
+
+const renderQuestions = function () {};
+
+// Start Quiz - remove elements, start timer and render question
+const startQuiz = function () {
+  startQuizDiv.remove();
+
+  renderTimer();
+
+  startTimer();
+
+  renderQuestions();
+};
+
+startQuizButton.addEventListener("click", startQuiz);
