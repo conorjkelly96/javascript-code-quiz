cc2c9c3813613bd5b52c561222af29437c9ddc4e
Conor Kelly <conorjkelly96@gmail.com>
2021-10-10 13:02:44 +0100

    pushing changes to highscore js

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,15 +1,15 @@
-let currentIndex = 0;
-
+// Render Highscore from Local Storage
 const highScoreList = function () {
   const latestScores = JSON.parse(localStorage.getItem("user-input") || "[]");
   console.log(latestScores);
+  let currentIndex = 0;
   const resultsCycle = latestScores[currentIndex];
   console.log(resultsCycle);
   console.log(latestScores[3].score);
 
   for (let i = 0; i < latestScores.length; i++) {
     console.log(i);
-    scoreList = document.createElement("p");
+    const scoreList = document.createElement("p");
     console.log(scoreList);
     scoreList.setAttribute("class", "score-tag");
     scoreList.setAttribute("id", "score-tag");
