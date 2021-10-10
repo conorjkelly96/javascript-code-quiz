91b362a1806b1d1278eb182621438bf61743b039
Conor Kelly <conorjkelly96@gmail.com>
2021-10-10 13:46:29 +0100

    pushing read me

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,20 +1,17 @@
 // Render Highscore from Local Storage
 const highScoreList = function () {
   const latestScores = JSON.parse(localStorage.getItem("user-input") || "[]");
-  // console.log(latestScores[16].initials, latestScores[1].score);
-  let currentIndex = 0;
-  let resultsCycle = latestScores[currentIndex];
-  // console.log(resultsCycle);
-  // console.log(latestScores[3].score);
 
   for (let i = 0; i < latestScores.length; i++) {
     console.log(i);
+    const data = latestScores[i];
+    console.log(data);
     const scoreList = document.createElement("p");
     console.log(scoreList);
     scoreList.setAttribute("class", "score-tag");
     scoreList.setAttribute("id", "score-tag");
-    scoreList.textContent = `Player Name: ${resultsCycle.initials} || Player Score: ${resultsCycle.score}`;
-    console.log(resultsCycle.initials);
+    scoreList.textContent = `Player Name: ${data.initials} || Player Score: ${data.score}`;
+    console.log(data.initials);
     const resultsDiv = document.getElementById("results-quiz-div");
     resultsDiv.appendChild(scoreList);
   }
