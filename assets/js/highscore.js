7f40c5b6beb120d6d34f859ce9bd1bba733408d7
Conor Kelly <conorjkelly96@gmail.com>
2021-11-12 20:35:35 +0000

    NM comment on load

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -2,15 +2,6 @@
 const highScoreList = function () {
   const latestScores = JSON.parse(localStorage.getItem("user-input") || "[]");
 
-  // for (let i = 0; i < latestScores.length; i++) {
-  //   const data = latestScores[i];
-  //   const scoreList = document.createElement("p");
-  //   scoreList.setAttribute("class", "score-tag");
-  //   scoreList.setAttribute("id", "score-tag");
-  //   scoreList.textContent = `Player Name: ${data.initials} || Player Score: ${data.score}`;
-  //   const resultsDiv = document.getElementById("results-quiz-div");
-  //   resultsDiv.appendChild(scoreList);
-  // }
   const constructLatestScores = function (latestScores) {
     const scoreList = document.createElement("p");
     scoreList.setAttribute("class", "score-tag");
@@ -23,6 +14,4 @@ const highScoreList = function () {
   latestScores.forEach(constructLatestScores);
 };
 
-highScoreList();
-
-// CK 10/10/2021: Could refactor to be an onLoad event rather than a function call?
+window.onLoad(highScoreList());
