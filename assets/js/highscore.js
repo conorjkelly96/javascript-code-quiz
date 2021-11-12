4d3ce9e779238a23ca631468863f9ffc8269b462
Conor Kelly <conorjkelly96@gmail.com>
2021-11-12 20:32:03 +0000

    commit+

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -2,15 +2,25 @@
 const highScoreList = function () {
   const latestScores = JSON.parse(localStorage.getItem("user-input") || "[]");
 
-  for (let i = 0; i < latestScores.length; i++) {
-    const data = latestScores[i];
+  // for (let i = 0; i < latestScores.length; i++) {
+  //   const data = latestScores[i];
+  //   const scoreList = document.createElement("p");
+  //   scoreList.setAttribute("class", "score-tag");
+  //   scoreList.setAttribute("id", "score-tag");
+  //   scoreList.textContent = `Player Name: ${data.initials} || Player Score: ${data.score}`;
+  //   const resultsDiv = document.getElementById("results-quiz-div");
+  //   resultsDiv.appendChild(scoreList);
+  // }
+  const constructLatestScores = function (latestScores) {
     const scoreList = document.createElement("p");
     scoreList.setAttribute("class", "score-tag");
     scoreList.setAttribute("id", "score-tag");
-    scoreList.textContent = `Player Name: ${data.initials} || Player Score: ${data.score}`;
+    scoreList.textContent = `Player Name: ${latestScores.initials} || Player Score: ${latestScores.score}`;
     const resultsDiv = document.getElementById("results-quiz-div");
     resultsDiv.appendChild(scoreList);
-  }
+  };
+
+  latestScores.forEach(constructLatestScores);
 };
 
 highScoreList();
